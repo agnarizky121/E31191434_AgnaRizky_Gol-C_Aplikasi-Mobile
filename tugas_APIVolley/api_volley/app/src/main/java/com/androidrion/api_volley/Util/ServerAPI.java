5e8871d63e0e52548b818320446a4917dc1005a7
Agna <agnarizky1@gmail.com>
2021-06-09 21:48:33 +0700

    Aplikasi_Mobile_APIVolley

---
+++ tugas_APIVolley/api_volley/app/src/main/java/com/androidrion/api_volley/Util/ServerAPI.java
@@ -0,0 +1,8 @@
+package com.androidrion.api_volley.Util;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.56.1/crud/Mahasiswa/Api";
+    public static final String URL_INSERT = "http://192.168.56.1/crud/Mahasiswa/ApiInsert";
+    public static final String URL_DELETE = "http://192.168.56.1/crud/Mahasiswa/ApiDelete";
+    public static final String URL_UPDATE  = "http://192.168.56.1/crud/Mahasiswa/ApiUpdate";
+}
